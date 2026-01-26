3d8ecfa181b93b762dfbd8bb9fb80bd1f3474d88
刘海鹏 <liuhp@webterren.com>
2026-01-26 15:56:13 +0800

    feat：增加eslint + prettier的配置冲突规则

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginVue from "eslint-plugin-vue"
+import { defineConfig } from "eslint/config"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-]);
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      // 禁用与Prettier冲突的eslint规则
+      ...prettierConfig.rules,
+      // 将违反 Prettier 规则（.prettierrc.js）的作为 ESLint 警告
+      "prettier/prettier": "warn",
+    },
+  },
+])
