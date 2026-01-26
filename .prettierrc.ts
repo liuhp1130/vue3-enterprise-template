da8f642da4e7e7846541ad36e0f2e4e6976ad804
刘海鹏 <liuhp@webterren.com>
2026-01-26 15:14:20 +0800

    feat:增加prettir代码格式化

---
+++ .prettierrc.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2, // tab 占两个空格
+  semi: false,  // 语句结尾的分号
+  singleQuote: false,  //使用单引号
+  trailingComma: "<all|es5|none>", // 末尾的逗号
+  bracketSpacing: true, // 文字与括号之间的空格 如{ foo: bar }
+  bracketSameLine: false, // html标签多行属性，将闭合‘>’符号换行在最后一行
+  arrowParens: "always", // 箭头函数单参数的括号是否省略，“always”：不省略；“avoid”省略
+  vueIndentScriptAndStyle:false, // 不在vue文件中缩进脚本和样式的标签
+  
+}
