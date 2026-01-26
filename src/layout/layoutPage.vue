11ed7ab48740fdfd73e97ed4e56490734339af43
刘海鹏 <liuhp@webterren.com>
2026-01-26 16:58:00 +0800

    feat:配置husky + lint-staged

--- src/layout/layoutPage.vue
+++ src/layout/layoutPage.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue"
+import HelloWorld from "@/components/HelloWorld.vue"
 let abc = 0
 const abc2 = ref(0)
 function add(a: number, b: number) {
-  console.log('emit', a + b);
+  console.log("emit", a + b)
   abc = a + b
   abc2.value = a + b
 }
@@ -21,5 +21,3 @@ function add(a: number, b: number) {
   </div>
   <HelloWorld msg="Vite + Vue" @add="add" :abc="abc" :abc2="abc2" />
 </template>
-
-
